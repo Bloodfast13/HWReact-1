88b3678fa6f416b6015a22e1e78501bc56aff64e
Serg <bloodfast13@gmail.com>
2020-02-10 15:04:50 +0200

    Updates

---
+++ precache-manifest.b1c236195e17829f4776a0fc831749e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834e019c5b8b7aef11e84e35aab42ab4",
+    "url": "/HWreactAPP1/index.html"
+  },
+  {
+    "revision": "8f056fc6ea3dcce57624",
+    "url": "/HWreactAPP1/static/css/main.de9d8212.chunk.css"
+  },
+  {
+    "revision": "ac78ec64a57cdf1d5f49",
+    "url": "/HWreactAPP1/static/js/2.12d055fd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/HWreactAPP1/static/js/2.12d055fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f056fc6ea3dcce57624",
+    "url": "/HWreactAPP1/static/js/main.bd15e431.chunk.js"
+  },
+  {
+    "revision": "e390ece2440396723aca",
+    "url": "/HWreactAPP1/static/js/runtime-main.1bf8d059.js"
+  },
+  {
+    "revision": "e45d9d1f63b5b15b9ebf933e7d9088d3",
+    "url": "/HWreactAPP1/static/media/252.e45d9d1f.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/HWreactAPP1/static/media/github-logo.76d344f0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HWReact-1/precache-manifest.6c363b3b4cd38f865f7553d17b74b97f.js"
+  "/HWreactAPP1/precache-manifest.b1c236195e17829f4776a0fc831749e2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HWReact-1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HWreactAPP1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
